6c187b71955470d490dc425c9602456e0f1d554d
Yahya Batulu <yahyabatulu00@gmail.com>
2023-09-16 00:28:35 +0300

    Added gorm autoCreateTime & autoUpdateTime

--- models/base.go
+++ models/base.go
@@ -8,7 +8,7 @@ import (
 
 type BaseModel struct {
 	ID        uint           `json:"id" gorm:"primaryKey"`
-	CreatedAt time.Time      `json:"createdAt,omitempty"`
-	UpdatedAt time.Time      `json:"updatedAt,omitempty"`
+	CreatedAt time.Time      `json:"createdAt,omitempty" gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `json:"updatedAt,omitempty" gorm:"autoUpdateTime"`
 	DeletedAt gorm.DeletedAt `json:"deletedAt,omitempty" gorm:"index"`
 }
