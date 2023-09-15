5c9650048295309d9ce75a8ee08fa779c1288e86
Yahya Batulu <yahyabatulu00@gmail.com>
2023-09-15 20:27:45 +0300

    Update exchange_rates.go
    Comment

--- routes/exchange_rates.go
+++ routes/exchange_rates.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// Handle api/exchange-rates endpoint.
 func ExchangeRates(ctx *fiber.Ctx) error {
 	exchangeRates := workers.GetExchangeRates()
 	return ctx.Status(200).JSON(exchangeRates)
