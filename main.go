f8574b13824602a4b19735e181d31bfbe14f312f
Yahya Batulu <46069238+ByPikod@users.noreply.github.com>
2023-10-10 14:34:40 +0300

    Router moved

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"github.com/ByPikod/go-crypto/controllers"
 	"github.com/ByPikod/go-crypto/core"
 	"github.com/ByPikod/go-crypto/helpers"
+	"github.com/ByPikod/go-crypto/middleware"
 	"github.com/ByPikod/go-crypto/models"
 	"github.com/ByPikod/go-crypto/repositories"
+	"github.com/ByPikod/go-crypto/services"
+	"github.com/ansrivas/fiberprometheus"
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/websocket"
 )
 
 // @title           Go Crypto
@@ -45,14 +52,78 @@ func main() {
 
 	helpers.LogInfo("Migrating completed.")
 
+	// Create fiber app
+	App := fiber.New()
+	App.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{
+			"code":    200,
+			"message": "Hello world!",
+		})
+	})
+
 	// Initialize http server and routes.
-	router := core.NewRouter(
-		repositories.NewExchangesRepository(),
-		repositories.NewUserRepository(db, config.AuthSecret),
-		repositories.NewWalletRepository(db),
+	var (
+		exchangesRepository = repositories.NewExchangesRepository()
+		userRepository      = repositories.NewUserRepository(db)
+		walletRepository    = repositories.NewWalletRepository(db)
 	)
 
-	router.Initialize()
-	router.App().Listen(config.Host + ":" + config.Listen)
+	// Business layer (Services)
+	var (
+		exchangesService = services.NewExchangeService(exchangesRepository)
+		userService      = services.NewUserService(userRepository)
+		walletService    = services.NewWalletService(walletRepository)
+	)
+
+	// Presentation layer (Controllers)
+	var (
+		exchangesController = controllers.NewExchangesController(exchangesService)
+		userController      = controllers.NewUserController(userService)
+		walletController    = controllers.NewWalletController(walletService, exchangesService)
+	)
+
+	// Metrics
+	prometheus := fiberprometheus.New("metrics")
+	prometheus.RegisterAt(App, "/metrics")
+	App.Use(prometheus.Middleware)
+
+	// Middlewares
+	var (
+		authMiddleware = middleware.NewAuthMiddleware(userService)
+	)
+
+	App.Use(cors.New(
+		cors.Config{
+			AllowHeaders:     "",
+			AllowOrigins:     "*",
+			AllowCredentials: true,
+			AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		},
+	))
+
+	// Websocket
+	ws := App.Group("/ws")
+	ws.Use(middleware.WebSocket) // Returns "426" if upgrade not provided.
+	ws.Get("/exchange-rates", websocket.New(exchangesController.WSExchangeRates))
+
+	// REST Api
+	api := App.Group("/api")
+	api.Get("/exchange-rates", exchangesController.ExchangeRates)
+
+	user := api.Group("/user")
+	user.Post("/register", middleware.Json, userController.Register)
+	user.Post("/login", middleware.Json, userController.Login)
+	user.Get("/me", authMiddleware.Auth, userController.Me)
+
+	wallet := user.Group("/wallet", authMiddleware.Auth, middleware.Json)
+	wallet.Post("/deposit", walletController.Deposit)
+	wallet.Post("/buy", walletController.Buy)
+	wallet.Post("/withdraw", walletController.Withdraw)
+	wallet.Post("/sell", walletController.Sell)
+	wallet.Get("/balance", walletController.Balance)
+
+	// 404
+	App.Use(helpers.NotFound)
+	App.Listen(config.Host + ":" + config.Listen)
 
 }
