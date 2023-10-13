fb77e3f69a345b8b81c46ced27548facb8bee94a
Yahya Batulu <46069238+ByPikod@users.noreply.github.com>
2023-10-13 16:54:29 +0300

    Revert "Delete linguist.go"
    This reverts commit b44d991e6742de6497a998f4a90dc13e536b47a4.

---
+++ scripts/linguist.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(`
+		This code makes GitHub detect
+		the language of the repository
+		as "Go". Since the actual code
+		is in branches.
+	`)
+
+	//
+	// www.yahyabatulu.com
+	//
+
+}
